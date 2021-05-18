392b4e6dac05d44772164c32de23fbcf52e490d0
Leonard <>
2021-05-18 13:02:57 +0800

    add Demo

---
+++ PaddleOCRDemo/PaddleOCRDemo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  PaddleOCRDemo
+//
+//  Created by Leonard on 2021/5/13.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
